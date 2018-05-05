cc9f76a1ee4f62c212058808b2b713ca359608e3
DigitalData <legoxavierlocketravers@gmail.com>
2018-05-06 01:14:37 +1000

    Redesign
    - Changing from Bootstrap to Materialize because of it's simplicity and intuitive style.
    - Began design on a new logo, unfinished.

--- user-interface/home/menu.js
+++ user-interface/home/menu.js
@@ -1,16 +1,43 @@
-var activeElement = document.getElementById('active');
-activeElement.style.backgroundColor = '#190019';
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
 
-function menuHover(x) {
-  x.style.backgroundColor = '#100010';
-  x.style.borderColor = '#180018';
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
+});
+
+const { shell } = require('electron')
+
+function openGithub() {
+  shell.openExternal('https://github.com/CodeDigital/botmyguy');
 }
 
-function menuNormal(x) {
-  x.style.borderColor = '#180018';
-  if(x == document.getElementById('active')){
-    x.style.backgroundColor = '#190019';
-  }else{
-    x.style.backgroundColor = '#210021';
-  }
-}
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+// $(document).ready(function () {
+//   $('.sidenav').sidenav();
+// });
+
+
+// var activeElement = document.getElementById('active');
+// activeElement.style.backgroundColor = '#190019';
+
+// function menuHover(x) {
+//   x.style.backgroundColor = '#100010';
+//   x.style.borderColor = '#180018';
+// }
+
+// function menuNormal(x) {
+//   x.style.borderColor = '#180018';
+//   if(x == document.getElementById('active')){
+//     x.style.backgroundColor = '#190019';
+//   }else{
+//     x.style.backgroundColor = '#210021';
+//   }
+// }
