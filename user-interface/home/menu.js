ca67001758e0ebacfcfdc6d3a7c93ff945ae1f3a
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-05-11 18:08:44 +1000

    Updating Template (Material Design)

--- user-interface/home/menu.js
+++ user-interface/home/menu.js
@@ -14,6 +14,52 @@ function openGithub() {
   shell.openExternal('https://github.com/CodeDigital/botmyguy');
 }
 
+var isConnecting = false;
+
+const {ipcRenderer} = require('electron');
+const fs = require('fs');
+const db = require('../../database/database.js');
+
+db.getSettings(function (settings) {
+  var userWelcome = document.getElementById('welcomeText');
+  var wText = userWelcome.innerHTML;
+  wText = wText.substring(0, 9) + settings.user;
+  userWelcome.innerHTML = wText;
+});
+
+function connect() {
+  if (!isConnecting) {
+    var connectIcon = document.getElementById('connectIcon');
+    var loadingCircle = document.getElementById('loadingCircle');
+
+    loadingCircle.className = "preloader-wrapper small active left";
+    connectIcon.style = "display:none;"
+    ipcRenderer.send('connect');
+    isConnecting = true;
+  }
+}
+
+ipcRenderer.on('connect:success', function (e) {
+  var connectButton = document.getElementById('cButton');
+  var disconnectButton = document.getElementById('dcButton');
+
+  connectButton.className = "btn btn-large green waves-effect right hide";
+  disconnectButton.className = "btn btn-large red waves-effect right";
+  isConnecting = false;
+});
+
+function disconnect() {
+  ipcRenderer.send('disconnect');
+}
+
+ipcRenderer.on('disconnect:success', function (e) {
+  var connectButton = document.getElementById('cButton');
+  var disconnectButton = document.getElementById('dcButton');
+
+  connectButton.className = "btn btn-large green waves-effect right";
+  disconnectButton.className = "btn btn-large red waves-effect right hide";
+});
+
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
